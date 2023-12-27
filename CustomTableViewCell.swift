f14ca9c1ae66441894659e690856a96e741391c5
grigorijsamujlov <130353326+grigorijsamujlov@users.noreply.github.com>
2023-12-27 20:29:20 +0100

    Create CustomTableViewCell.swift

---
+++ CustomTableViewCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
